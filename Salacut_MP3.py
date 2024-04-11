c7156a86807c5419fbad9a8b96ba3904a1b1a526
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-04-11 15:25:57 +0800

    print Hello World

---
+++ Salacut_MP3.py
@@ -0,0 +1,16 @@
+'''
+HONOR CODE
+I declare, upon my honor, that I did this machine problem assignment by myself using references from the lecture notes and MPs.
+
+I have watched the video primer and decided to follow its lead
+
+REPO LINK
+
+NOTE: THE COMMIT MESSAGES AND REPO NAME ARE NOT FUNCTIONAL
+https://github.com/FranceRafaelSalacut/EMPI_TREE_AUTOMATA.git
+
+'''
+
+
+print("Hello World")
+
