dc7e23f994d0b1261f120089830954f7c522c66b
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-04-12 12:18:47 +0800

    committing so i have something to go back to in case everything fails

--- Salacut_MP3.py
+++ Salacut_MP3.py
@@ -11,10 +11,69 @@ https://github.com/FranceRafaelSalacut/EMPI_TREE_AUTOMATA.git
 
 '''
 
+import tokenize
+from io import BytesIO
+import re
+
+#DataType
+D_TYPE = ["int",
+        "char",
+        "float",
+        "double",
+        "void"
+        ]
+
+
+def manual_tokenize_(str):
+    tokens = []
+    delimiter = [',', ';', ' ']
+
+    token = ''
+    for char in str:
+        if char in delimiter:
+            if token != '':
+                tokens.append(token)
+            tokens.append(char)
+            token = ''
+        else:
+            token = token + char
+
+    return tokens
+
+def valid_name(name):
+    if name in D_TYPE:
+        return False
+    
+    if name[0].isdigit():
+        return False
+    
+    return True
+
+def valid_integer(str):
+    return str.isdigit()
+
+def valid_character(str):
+    return str.isalnum()
+
+def check_var_declaration(str):
+    print(str)
+
+    return False
+    
+
+def check_fun_declaration(str):
+    print(str)
+
+    return False
 
 
 def main():
-    print("Hello World")
+    i = int(input())
 
+    for _ in range(0, i):
+        word = manual_tokenize_(input()) # the tokenizer removes any extra spaces in the process
+        
+        result = check_var_declaration(word[2:]) if word[0] == "1" else check_fun_declaration(word[2:])
+
+        print(result)
 main()
-
---
+++ testtt.py
@@ -0,0 +1,6 @@
+
+
+str1 = ["1234", 'a', '1', "1asd1", 1, 123]
+
+for x in str1:
+    print(x.isalnum())
